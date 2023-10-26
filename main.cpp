3db57b0a69c1cd9575e6ed6fe7a4d93b9a405015
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-25 21:15:38 -0700

    first version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <stdexcept>
 
 using namespace std;
 
 int main()
 {
+    const static string ERRORIllegalOperation = "ERROR \nIllegal operation";
     int num1, num2;
     char opr;
 
@@ -19,26 +24,45 @@ int main()
 
     cout << num1 << " " << opr << " " << num2 << " = ";
 
-    switch (opr)
-    {
-        case '+':
-            cout << num1 + num2 << endl;
-            break;
-        case '-':
-            cout << num1 - num2 << endl;
-            break;
-        case '*':
-            cout << num1 * num2 << endl;
-            break;
-        case '/':
-            if (num2 != 0)
-                cout << num1 / num2 << endl;
-            else
-                cout << "ERROR \nCannot divide by zero" << endl;
-            break;
-        default:
-            cout << "Illegal operation" << endl;
+    try{
+        switch (opr)
+        {
+            case '+':
+                cout << num1 + num2 << endl;
+                break;
+            case '-':
+                cout << num1 - num2 << endl;
+                break;
+            case '*':
+                cout << num1 * num2 << endl;
+                break;
+            case '/':
+                if (num2 != 0)
+                    cout << num1 / num2 << endl;
+                else{
+                    char *i = new char[100];
+                    sprintf(i, "ERROR \nCannot divide by zero: %i/%i", num1, num2);
+                    throw i;
+                    // throw sprintf(new char[100], "ERROR \nCannot divide by zero: %i/%i", num1, num2);
+                }
+
+//                cout << "ERROR \nCannot divide by zero" << endl;
+                break;
+            default:
+                throw ERRORIllegalOperation;
+                // cout << "Illegal operation" << endl;
+        }
+    }catch (string& error){
+        cout << error << endl;
+    }catch (char *error){
+        cout << error << endl;
     }
 
     return 0;
 }
+/*
+structions:
+Redo Programming Exercise 8 of Chapter 4
+ so that your program handles exceptions such as division by zero and invalid input.
+Your program should print Division by zero when 0 is entered for a denominator.
+ */
